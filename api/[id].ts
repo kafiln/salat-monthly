7ebaa2443d11eb674c42afe6165c64671aa9d156
Kafil NASDAMI <nasdami.kafil@bcg.com>
2023-07-02 23:42:57 +0100

    allow cors

--- api/[id].ts
+++ api/[id].ts
@@ -3,9 +3,7 @@ import { getMonthlyPrayers } from "../utils";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { id } = req.query;
-  console.log(id);
   const data = await getMonthlyPrayers(Number(id));
-  console.log(data);
   return res.json({
     data,
   });
